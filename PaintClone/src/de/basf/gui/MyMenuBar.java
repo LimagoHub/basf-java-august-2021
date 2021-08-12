f006e35e9a1e9b2d496ce8f075ed984f5254c319
Jo Wagner <jowagner@limago.de>
2021-08-12 13:41:20 +0200

    Initial Commit

--- PaintClone/src/de/basf/gui/MyMenuBar.java
+++ PaintClone/src/de/basf/gui/MyMenuBar.java
@@ -11,13 +11,34 @@ public class MyMenuBar extends MenuBar {
         menu = new Menu("Datei");
         menuItem = new MenuItem("beenden");
         menu.add(menuItem);
-        menu.addSeparator();
+
         add(menu);
 
         menu = new Menu("Formen");
+        menuItem = new MenuItem("Linie");
+        menu.add(menuItem);
+        menuItem = new MenuItem("Rechteck");
+        menu.add(menuItem);
+        menuItem = new MenuItem("Oval");
+        menu.add(menuItem);
+        menu.addSeparator();
+        menuItem = new MenuItem("Löschen");
+        menu.add(menuItem);
         add(menu);
 
         menu = new Menu("Farben");
+        menuItem = new MenuItem("weiss");
+        menu.add(menuItem);
+        menuItem = new MenuItem("rot");
+        menu.add(menuItem);
+        menuItem = new MenuItem("grün");
+        menu.add(menuItem);
+        menuItem = new MenuItem("gelb");
+        menu.add(menuItem);
+        menuItem = new MenuItem("blau");
+        menu.add(menuItem);
+        menuItem = new MenuItem("pink");
+        menu.add(menuItem);
         add(menu);
 
     }
